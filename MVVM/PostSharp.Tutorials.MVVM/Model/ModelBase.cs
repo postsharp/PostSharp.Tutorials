d8e08bc9d3b01b66399eba74a7cb2499ce6b6c54
Gael Fraiteur <gael@postsharp.net>
2020-07-06 12:27:20 +0200

    Remove irrelevant comment.

--- MVVM/PostSharp.Tutorials.MVVM/Model/ModelBase.cs
+++ MVVM/PostSharp.Tutorials.MVVM/Model/ModelBase.cs
@@ -2,9 +2,6 @@
 
 namespace PostSharp.Tutorials.MVVM.Model
 {
-    // We're adding two aspects to the base class of all Model classes and the aspects
-    // will be automatically added by all children classes.
-
     [NotifyPropertyChanged]
     public abstract class ModelBase
     {
